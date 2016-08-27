927d3b56a016be698284009800180378bcb73c2f
STEFAN SCORSE <sirix.krane@gmail.com>
2016-08-27 18:11:32 +1000

    test123

--- pythonfun.py
+++ pythonfun.py
@@ -1,9 +1,7 @@
 print("hello world")
 print("hello world")
 
-<<<<<<< HEAD
 print("what happens?")
-=======
 
 print("hello world")
 print("hello world")
@@ -11,4 +9,3 @@ print("hello world")
 
 print("hello world")
 print("hello world")
->>>>>>> 4a9c826d454ae629ab6af2f35fdf234acadcd946
